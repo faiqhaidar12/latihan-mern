55a6bd82d8db4c718a0dad5cec151b6d2f9eddbc
faiqhaidar12 <faiqhaidar1@gmail.com>
2022-10-24 13:51:30 +0700

    edit api

--- mern-api/index.js
+++ mern-api/index.js
@@ -4,9 +4,6 @@ const app = express();
 
 app.use(() => {
   console.log("hello server");
-  console.log("hello server lagi");
-  console.log("hello server lagiiiii");
-  console.log("hello server lagiiiiis");
 });
 
 app.listen(4000);
