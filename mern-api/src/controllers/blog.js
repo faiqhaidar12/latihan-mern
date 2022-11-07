78dd0abf3c8772fa5761c22720fedd72e7c2eb72
faiqhaidar12 <faiqhaidar1@gmail.com>
2022-11-07 10:06:00 +0700

    update postId

--- mern-api/src/controllers/blog.js
+++ mern-api/src/controllers/blog.js
@@ -42,3 +42,80 @@ exports.createBlogPost = (req, res, next) => {
       console.log("err: ", err);
     });
 };
+
+exports.getAllBlogPost = (req, res, next) => {
+  BlogPost.find()
+    .then((result) => {
+      res.status(200).json({
+        message: "Data Blog Post Berhasil dipanggil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getBlogPostById = (req, res, next) => {
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then((result) => {
+      if (!result) {
+        const error = new Error("Blog Post tidak ditemukan!");
+        error.errorStatus = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: "Data Blog Post Berhasil dipanggil!",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.updateBlogPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error("Input Value Tidak Sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  if (!req.file) {
+    const err = new Error("Image Harus di Upload");
+    err.errorStatus = 422;
+    throw err;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path;
+  const body = req.body.body;
+  const post = req.params.postId;
+
+  BlogPost.findById(post)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Blog Post Tidak ditemukan!");
+        err.errorStatus = 404;
+        throw err;
+      }
+      post.title = title;
+      post.body = body;
+      post.image = image;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(202).json({
+        message: "Update Blog Post Berhasil!",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
--- mern-api/src/routes/blog.js
+++ mern-api/src/routes/blog.js
@@ -2,8 +2,9 @@ const express = require("express");
 const { body } = require("express-validator");
 
 const router = express.Router();
-const blogControllerPost = require("../controllers/blog");
+const blogController = require("../controllers/blog");
 
+//[POST] : /v1/blog/post
 router.post(
   "/post",
   [
@@ -14,7 +15,21 @@ router.post(
       .isLength({ min: 5 })
       .withMessage("Minimal Input Body 5 Karakter"),
   ],
-  blogControllerPost.createBlogPost
+  blogController.createBlogPost
 );
-//[POST] : /v1/blog/post
+router.get("/posts", blogController.getAllBlogPost);
+router.get("/post/:postId", blogController.getBlogPostById);
+router.put(
+  "/post/:postId",
+  [
+    body("title")
+      .isLength({ min: 2 })
+      .withMessage("Minimal Input Title 5 Karakter"),
+    body("body")
+      .isLength({ min: 5 })
+      .withMessage("Minimal Input Body 5 Karakter"),
+  ],
+  blogController.updateBlogPost
+);
+
 module.exports = router;
