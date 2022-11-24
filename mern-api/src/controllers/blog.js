30e53ca964cc193f34d0a75468b4b504a68617b5
faiqhaidar12 <faiqhaidar1@gmail.com>
2022-11-24 12:14:58 +0700

    remove image

--- mern-api/src/controllers/blog.js
+++ mern-api/src/controllers/blog.js
@@ -1,4 +1,7 @@
 const { validationResult } = require("express-validator");
+const path = require("path");
+const fs = require("fs");
+
 const BlogPost = require("../models/blog");
 
 exports.createBlogPost = (req, res, next) => {
@@ -44,11 +47,25 @@ exports.createBlogPost = (req, res, next) => {
 };
 
 exports.getAllBlogPost = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = req.query.perPage || 5;
+  let totalItems;
+
   BlogPost.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return BlogPost.find()
+        .skip(parseInt(currentPage - 1) * parseInt(perPage))
+        .limit(parseInt(perPage));
+    })
     .then((result) => {
       res.status(200).json({
         message: "Data Blog Post Berhasil dipanggil",
         data: result,
+        total_data: totalItems,
+        per_page: parseInt(perPage),
+        current_page: parseInt(currentPage),
       });
     })
     .catch((err) => {
@@ -118,4 +135,36 @@ exports.updateBlogPost = (req, res, next) => {
     .catch((err) => {
       next(err);
     });
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Blog Post Tidak ditemukan!");
+        err.errorStatus = 404;
+        throw err;
+      }
+      removeImage(post.image);
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Hapus Blog Berhasil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const removeImage = (filePath) => {
+  console.log("filepath", filePath);
+  console.log("dir name", __dirname);
+
+  filePath = path.join(__dirname, "../..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
